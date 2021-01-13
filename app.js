1a3eea6714a400445bd3157084a61ff4539f09f3
Dylan White <dylan.whitej@gmail.com>
2021-01-13 14:29:37 -0800

    grabs DOM elements and initializes state

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // import functions and grab DOM elements
+import { didUserWin } from './utils.js';
+import { getRandomThrow } from './get-random-throw.js';
+
+const winNumber = document.getElementById('win-number');
+const lossNumber = document.getElementById('loss-number');
+const drawNumber = document.getElementById('draw-number');
+const computerImage = document.getElementById('computer-image');
+const userImage = document.getElementById('user-image');
+const throwButton = document.getElementById('throw-button');
+const resetButton = document.getElementById('reset-button');
 
 // initialize state
+let numberOfWins = 0;
+let numberOfLosses = 0;
+
 
 // set event listeners to update state and DOM
