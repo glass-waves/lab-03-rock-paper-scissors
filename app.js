1745d6d285da8656e07d38ad951fda44a3ff9136
Dylan White <dylan.whitej@gmail.com>
2021-01-13 15:14:50 -0800

    adds function for displaying winner text

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // import functions and grab DOM elements
 import { didUserWin } from './utils.js';
 import { getRandomThrow } from './get-random-throw.js';
+import { displayPhoto, displayWhoWins } from './displayFunctions.js';
+
 
 const winNumber = document.getElementById('win-number');
 const lossNumber = document.getElementById('loss-number');
 const drawNumber = document.getElementById('draw-number');
-const computerImage = document.getElementById('computer-image');
-const userImage = document.getElementById('user-image');
 const throwButton = document.getElementById('throw-button');
 const resetButton = document.getElementById('reset-button');
 
@@ -16,3 +16,17 @@ let numberOfLosses = 0;
 
 
 // set event listeners to update state and DOM
+throwButton.addEventListener('click', () => {
+
+    const userChoice = document.querySelector('input[type="radio"]:checked');
+    const userThrow = userChoice.value;
+    const computerThrow = getRandomThrow();
+
+    //display user image
+    displayPhoto(userThrow, computerThrow);
+    //tabulate the results    
+    const result = didUserWin(userThrow, computerThrow);
+    //display who wins
+    displayWhoWins(result);
+
+});
